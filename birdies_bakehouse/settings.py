8484bc10c0a8bf46ae30e167df8cb68264817c71
haleanana <haleanana@gmail.com>
2021-03-28 09:23:18 +0000

    Added css for products page

--- birdies_bakehouse/settings.py
+++ birdies_bakehouse/settings.py
@@ -151,5 +151,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# Media Setting
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
