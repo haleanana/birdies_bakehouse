fa79eba4f0bdfc1db2c8b5843b85d55fc522d360
haleanana <haleanana@gmail.com>
2021-03-28 14:46:41 +0000

    Fixed styles and added search bar

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
 from .models import *
 
 
@@ -7,8 +8,20 @@ from .models import *
 def all_products(request):
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not enter any search criteria")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query, 
     }
 
     return render(request, 'products/products.html', context)
