307d0dd9703ea4b3122feb11dab500cc2344a0f7
haleanana <haleanana@gmail.com>
2021-03-29 20:27:01 +0000

    Added adjust and remove feature from shopping bag

--- bag/views.py
+++ bag/views.py
@@ -21,3 +21,44 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+
+    """Remove the item from the shopping bag"""
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        info = None
+
+        if 'product_info' in request.POST:
+            size = request.POST['product_info']
+            bag = request.session.get('bag', {})
+
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag!')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
