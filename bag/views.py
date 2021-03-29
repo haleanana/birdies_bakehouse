7c37f58937f6fa7450ecfa60ee38a6ecab847f18
haleanana <haleanana@gmail.com>
2021-03-29 18:27:11 +0000

    Add to bag functionality added

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 
 # Create your views here.
 def view_bag(request):
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        # messages.success(
+        #     request, f'Updated {product.name} to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        # messages.success(request, f'Added {product.name} to your bag! ')
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
